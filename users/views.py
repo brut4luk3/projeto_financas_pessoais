770806975af2bb94a967a53d58927de65c303285
brut4luk3 <lucasreinert96@gmail.com>
2022-05-18 22:12:47 -0300

    Nova app - accounts

--- users/views.py
+++ users/views.py
@@ -1,8 +1,18 @@
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
+
+from accounts.models import Account
+from .models import User
 
 def index(request):
 
-    return render(request, 'users/index.html')
+    total_num_accounts = request.user.account_set.all().count()
+
+    context = {
+        'total_num_accounts': total_num_accounts
+    }
+
+    return render(request, 'users/index.html', context=context)
 
 
 def detail(request, user_id):
