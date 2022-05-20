8cba03eacad105da83c92d498c2ef24745c20d1e
brut4luk3 <lucasreinert96@gmail.com>
2022-05-19 22:10:25 -0300

    Nova app - accounts

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 from accounts.models import Account
 from .models import User
@@ -15,6 +16,43 @@ def index(request):
     return render(request, 'users/index.html', context=context)
 
 
-def detail(request, user_id):
+def detail(request: HttpRequest, user_id: int):
 
     return render(request, 'users/detail.html')
+
+
+def create_account(request):
+
+    if request.method == 'GET':
+        return render(request, 'accounts/create.html')
+
+    elif request.method == 'POST':
+
+        post_data = request.POST
+
+        if len(post_data.get("txtInitialBalance").strip()):
+            balance = float(post_data.get("txtInitialBalance").strip())
+
+        else:
+            balance = 0
+
+        account = Account(
+            user_id=request.user,
+            name=post_data.get('txtAccountName'),
+            balance=balance
+        )
+
+        account.save()
+
+        return HttpResponseRedirect(reverse('users:list_accounts',))
+
+
+def list_accounts(request):
+
+    user_accounts = request.user.account_set.all()
+
+    context = {
+        'user_accounts': user_accounts
+    }
+
+    return render(request, 'accounts/list.html', context=context)
